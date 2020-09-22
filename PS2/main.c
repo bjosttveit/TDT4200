20830413165076b5e7e39b6e5db640aa325fc325
Bjørnar Østtveit <bjosttveit@gmail.com>
2020-09-22 09:31:52 +0200

    started planning

--- PS2/main.c
+++ PS2/main.c
@@ -4,81 +4,92 @@
 #include <getopt.h>
 #include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
 #include "libs/bitmap.h"
 
 // Convolutional Kernel Examples, each with dimension 3,
 // gaussian kernel with dimension 5
 
 int sobelYKernel[] = {-1, -2, -1,
-                       0,  0,  0,
-                       1,  2,  1};
+                      0, 0, 0,
+                      1, 2, 1};
 
 int sobelXKernel[] = {-1, -0, 1,
-                      -2,  0, 2,
-                      -1,  0, 1};
+                      -2, 0, 2,
+                      -1, 0, 1};
 
-int laplacian1Kernel[] = { -1,  -4,  -1,
-                           -4,  20,  -4,
-                           -1,  -4,  -1};
+int laplacian1Kernel[] = {-1, -4, -1,
+                          -4, 20, -4,
+                          -1, -4, -1};
 
-int laplacian2Kernel[] = { 0,  1,  0,
-                           1, -4,  1,
-                           0,  1,  0};
+int laplacian2Kernel[] = {0, 1, 0,
+                          1, -4, 1,
+                          0, 1, 0};
 
-int laplacian3Kernel[] = { -1,  -1,  -1,
-                           -1,   8,  -1,
-                           -1,  -1,  -1};
+int laplacian3Kernel[] = {-1, -1, -1,
+                          -1, 8, -1,
+                          -1, -1, -1};
 
-int gaussianKernel[] = { 1,  4,  6,  4, 1,
-                         4, 16, 24, 16, 4,
-                         6, 24, 36, 24, 6,
-                         4, 16, 24, 16, 4,
-                         1,  4,  6,  4, 1 };
+int gaussianKernel[] = {1, 4, 6, 4, 1,
+                        4, 16, 24, 16, 4,
+                        6, 24, 36, 24, 6,
+                        4, 16, 24, 16, 4,
+                        1, 4, 6, 4, 1};
 
-char* const kernelNames[]       = { "SobelY",     "SobelX",     "Laplacian 1",    "Laplacian 2",    "Laplacian 3",    "Gaussian"     };
-int* const kernels[]            = { sobelYKernel, sobelXKernel, laplacian1Kernel, laplacian2Kernel, laplacian3Kernel, gaussianKernel };
-unsigned int const kernelDims[] = { 3,            3,            3,                3,                3,                5              };
-float const kernelFactors[]     = { 1.0,          1.0,          1.0,              1.0,              1.0,              1.0 / 256.0    };
+char *const kernelNames[] = {"SobelY", "SobelX", "Laplacian 1", "Laplacian 2", "Laplacian 3", "Gaussian"};
+int *const kernels[] = {sobelYKernel, sobelXKernel, laplacian1Kernel, laplacian2Kernel, laplacian3Kernel, gaussianKernel};
+unsigned int const kernelDims[] = {3, 3, 3, 3, 3, 5};
+float const kernelFactors[] = {1.0, 1.0, 1.0, 1.0, 1.0, 1.0 / 256.0};
 
 int const maxKernelIndex = sizeof(kernelDims) / sizeof(unsigned int);
 
-
 // Helper function to swap bmpImageChannel pointers
 
-void swapImage(bmpImage **one, bmpImage **two) {
+void swapImage(bmpImage **one, bmpImage **two)
+{
   bmpImage *helper = *two;
   *two = *one;
   *one = helper;
 }
 
 // Apply convolutional kernel on image data
-void applyKernel(pixel **out, pixel **in, unsigned int width, unsigned int height, int *kernel, unsigned int kernelDim, float kernelFactor) {
+void applyKernel(pixel **out, pixel **in, unsigned int width, unsigned int height, int *kernel, unsigned int kernelDim, float kernelFactor)
+{ //Take in halo pointers and thiccness
   unsigned int const kernelCenter = (kernelDim / 2);
-  for (unsigned int y = 0; y < height; y++) {
-    for (unsigned int x = 0; x < width; x++) {
+  for (unsigned int y = 0; y < height; y++)
+  {
+    for (unsigned int x = 0; x < width; x++)
+    {
       unsigned int ar = 0, ag = 0, ab = 0;
-      for (unsigned int ky = 0; ky < kernelDim; ky++) {
+      for (unsigned int ky = 0; ky < kernelDim; ky++)
+      {
         int nky = kernelDim - 1 - ky;
-        for (unsigned int kx = 0; kx < kernelDim; kx++) {
+        for (unsigned int kx = 0; kx < kernelDim; kx++)
+        {
           int nkx = kernelDim - 1 - kx;
 
           int yy = y + (ky - kernelCenter);
           int xx = x + (kx - kernelCenter);
-          if (xx >= 0 && xx < (int) width && yy >=0 && yy < (int) height) {
+          if (xx >= 0 && xx < (int)width && yy >= 0 && yy < (int)height)
+          {
             ar += in[yy][xx].r * kernel[nky * kernelDim + nkx];
             ag += in[yy][xx].g * kernel[nky * kernelDim + nkx];
             ab += in[yy][xx].b * kernel[nky * kernelDim + nkx];
           }
+          //ELSE CONDITION USE HALO VALUE
         }
       }
-      if (ar || ag || ab) {
+      if (ar || ag || ab)
+      {
         ar *= kernelFactor;
         ag *= kernelFactor;
         ab *= kernelFactor;
         out[y][x].r = (ar > 255) ? 255 : ar;
         out[y][x].g = (ag > 255) ? 255 : ag;
         out[y][x].b = (ab > 255) ? 255 : ab;
-      } else {
+      }
+      else
+      {
         out[y][x].r = 0;
         out[y][x].g = 0;
         out[y][x].b = 0;
@@ -87,28 +98,54 @@ void applyKernel(pixel **out, pixel **in, unsigned int width, unsigned int heigh
   }
 }
 
+void processRow(bmpImage *out, bmpImage *in, int kernelIndex, int my_rank, int comm_sz, int iterations)
+{
+  for (unsigned int i = 0; i < iterations; i++)
+  {
 
-void help(char const *exec, char const opt, char const *optarg) {
-    FILE *out = stdout;
-    if (opt != 0) {
-        out = stderr;
-        if (optarg) {
-            fprintf(out, "Invalid parameter - %c %s\n", opt, optarg);
-        } else {
-            fprintf(out, "Invalid parameter - %c\n", opt);
-        }
-    }
-    fprintf(out, "%s [options] <input-bmp> <output-bmp>\n", exec);
-    fprintf(out, "\n");
-    fprintf(out, "Options:\n");
-    fprintf(out, "  -k, --kernel     <kernel>        kernel index (0<=x<=%u) (2)\n", maxKernelIndex -1);
-    fprintf(out, "  -i, --iterations <iterations>    number of iterations (1)\n");
+    //Exchange halos
 
-    fprintf(out, "\n");
-    fprintf(out, "Example: %s before.bmp after.bmp -i 10000\n", exec);
+    applyKernel(out->data,
+                in->data,
+                in->width,
+                in->height,
+                kernels[kernelIndex],
+                kernelDims[kernelIndex],
+                kernelFactors[kernelIndex]);
+                //NorthHaloPointer
+                //SouthHaloPointer
+
+    swapImage(out, in);
+  }
 }
 
-int main(int argc, char **argv) {
+void help(char const *exec, char const opt, char const *optarg)
+{
+  FILE *out = stdout;
+  if (opt != 0)
+  {
+    out = stderr;
+    if (optarg)
+    {
+      fprintf(out, "Invalid parameter - %c %s\n", opt, optarg);
+    }
+    else
+    {
+      fprintf(out, "Invalid parameter - %c\n", opt);
+    }
+  }
+  fprintf(out, "%s [options] <input-bmp> <output-bmp>\n", exec);
+  fprintf(out, "\n");
+  fprintf(out, "Options:\n");
+  fprintf(out, "  -k, --kernel     <kernel>        kernel index (0<=x<=%u) (2)\n", maxKernelIndex - 1);
+  fprintf(out, "  -i, --iterations <iterations>    number of iterations (1)\n");
+
+  fprintf(out, "\n");
+  fprintf(out, "Example: %s before.bmp after.bmp -i 10000\n", exec);
+}
+
+int main(int argc, char **argv)
+{
   /*
     Parameter parsing, don't change this!
    */
@@ -118,35 +155,38 @@ int main(int argc, char **argv) {
   unsigned int kernelIndex = 2;
   int ret = 0;
 
-  static struct option const long_options[] =  {
-      {"help",       no_argument,       0, 'h'},
-      {"kernel",     required_argument, 0, 'k'},
+  static struct option const long_options[] = {
+      {"help", no_argument, 0, 'h'},
+      {"kernel", required_argument, 0, 'k'},
       {"iterations", required_argument, 0, 'i'},
-      {0, 0, 0, 0}
-  };
+      {0, 0, 0, 0}};
 
-  static char const * short_options = "hk:i:";
+  static char const *short_options = "hk:i:";
   {
     char *endptr;
     int c;
     int parse;
     int option_index = 0;
-    while ((c = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1) {
-      switch (c) {
+    while ((c = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1)
+    {
+      switch (c)
+      {
       case 'h':
-        help(argv[0],0, NULL);
+        help(argv[0], 0, NULL);
         goto graceful_exit;
       case 'k':
         parse = strtol(optarg, &endptr, 10);
-        if (endptr == optarg || parse < 0 || parse >= maxKernelIndex) {
+        if (endptr == optarg || parse < 0 || parse >= maxKernelIndex)
+        {
           help(argv[0], c, optarg);
           goto error_exit;
         }
-        kernelIndex = (unsigned int) parse;
+        kernelIndex = (unsigned int)parse;
         break;
       case 'i':
         iterations = strtol(optarg, &endptr, 10);
-        if (endptr == optarg) {
+        if (endptr == optarg)
+        {
           help(argv[0], c, optarg);
           goto error_exit;
         }
@@ -157,8 +197,9 @@ int main(int argc, char **argv) {
     }
   }
 
-  if (argc <= (optind+1)) {
-    help(argv[0],' ',"Not enough arugments");
+  if (argc <= (optind + 1))
+  {
+    help(argv[0], ' ', "Not enough arugments");
     goto error_exit;
   }
 
@@ -176,17 +217,27 @@ int main(int argc, char **argv) {
     End of Parameter parsing!
    */
 
+  int comm_sz;
+  int my_rank;
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
   /*
     Create the BMP image and load it from disk.
    */
-  bmpImage *image = newBmpImage(0,0);
-  if (image == NULL) {
+  //START RANK 1==========================
+
+  bmpImage *image = newBmpImage(0, 0);
+  if (image == NULL)
+  {
     fprintf(stderr, "Could not allocate new image!\n");
     goto error_exit;
   }
 
-  if (loadBmpImage(image, input) != 0) {
+  if (loadBmpImage(image, input) != 0)
+  {
     fprintf(stderr, "Could not load bmp image '%s'!\n", input);
     freeBmpImage(image);
     goto error_exit;
@@ -194,37 +245,39 @@ int main(int argc, char **argv) {
 
   printf("Apply kernel '%s' on image with %u x %u pixels for %u iterations\n", kernelNames[kernelIndex], image->width, image->height, iterations);
 
-
-  // TODO: implement time measurement from here
+  double startTime = MPI_Wtime();
 
   // Here we do the actual computation!
   // image->data is a 2-dimensional array of pixel which is accessed row first ([y][x])
   // each pixel is a struct of 3 unsigned char for the red, blue and green colour channel
   bmpImage *processImage = newBmpImage(image->width, image->height);
-  for (unsigned int i = 0; i < iterations; i ++) {
-    applyKernel(processImage->data,
-                image->data,
-                image->width,
-                image->height,
-                kernels[kernelIndex],
-                kernelDims[kernelIndex],
-                kernelFactors[kernelIndex]
-                );
-    swapImage(&processImage, &image);
-  }
+
+  //Distribute rows to bmps
+
+  //END RANK 1========================
+
+  //MPI_SCATTER
+
+  //processRow
+  
   freeBmpImage(processImage);
 
-  // TODO: implement time measurement to here
-  double spentTime = 0.0;
+  //MPI_GATHER
+
+  //START RANK 1=======================
+
+  double spentTime = MPI_Wtime() - startTime;
   printf("Time spent: %.3f seconds\n", spentTime);
 
- 
   //Write the image back to disk
-  if (saveBmpImage(image, output) != 0) {
+  if (saveBmpImage(image, output) != 0)
+  {
     fprintf(stderr, "Could not save output to '%s'!\n", output);
     freeBmpImage(image);
     goto error_exit;
   };
+
+  //END RANK 1=========================
 
 graceful_exit:
   ret = 0;
@@ -233,5 +286,6 @@ error_exit:
     free(input);
   if (output)
     free(output);
+  MPI_Finalize();
   return ret;
 };
